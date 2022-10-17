bdb96f1a5bd749a23b763306b875348e2e92df57
Nanang Puji <nanangpuji762@gmail.com>
2022-10-17 18:28:44 +0700

    Koding QuickShort

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,13 +1,13 @@
-﻿using System.Xml;
+﻿using System;
 
-namespace Quicksort
+namespace QuickShort
 {
-    class program
+    class Program
     {
         //array of integers to hold values
         private int[] arr = new int[20];
-        private int cmp_count = 0;// number of comparasion
-        private int mov_count = 0;// number of data movements
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
 
         //number of element in array
         private int n;
@@ -16,17 +16,17 @@ namespace Quicksort
         {
             while (true)
             {
-                Console.Write("Enter the number of element in the array :");
+                Console.Write("Enter the number of elements in the array :");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
                 if (n <= 20)
                     break;
                 else
-                    Console.WriteLine("\nArray acn have maximum 20 elements \n");
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
             }
-            Console.WriteLine("\n=====================");
+            Console.WriteLine("\n======================");
             Console.WriteLine("Enter Array Elements");
-            Console.WriteLine("\n=====================");
+            Console.WriteLine("\n======================");
 
             //get array elements
             for (int i = 0; i < n; i++)
@@ -44,8 +44,89 @@ namespace Quicksort
             arr[x] = arr[y];
             arr[y] = temp;
         }
-        public void q_sort(int low,int high)
+        public void q_sort(int low, int high)
         {
+            int pivot, i, j;
+            if (low > high)
+                return;
 
+            //partition the list into two parts;
+            //one containing elements less that or equal to pivot
+            //outher containning elements greater than pivot
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //search for an element greater that pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+                while ((arr[j] > pivot) && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j) //if the greater element is on the left of the element
+                {
+                    //swap the elements at index i with the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
+            //j now contains the index of the last element in the sorted list
+
+            if (low < j)
+            {
+                //move the pivot to its correct position in the list
+                swap(low, j);
+                mov_count++;
+            }
+            // sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
         }
+        void display()
+        {
+            Console.WriteLine("\n---------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-----------------------");
 
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
+        }
+        int getSize()
+        {
+            return (n);
+        }
+        static void Main(string[] args)
+        {
+            //declaring the object of the class
+            Program myList = new Program();
+            //accept array elements
+            myList.read();
+            //calling the sorting function
+            //first call to quick sort algorithm
+            myList.q_sort(0, myList.getSize() - 1);
+            //display sorted array 
+            myList.display();
+            //to exit from the console
+            Console.WriteLine("\n\nPress Enter to exit");
+            Console.Read();
+        }
+    }
+}
